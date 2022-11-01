5f8cf4f68474422aa5db3bff6bfaea4dc7f19780
kyw5lien <100414412+kyw5lien@users.noreply.github.com>
2022-11-01 04:45:50 -0100

    Create ICustomerService.java
    Create ICustomerService.java

---
+++ ICustomerService.java
@@ -0,0 +1,8 @@
+package model;
+
+public interface ICustomerService{
+    public String getCustomerServiceSRVC();
+    public Double getCustomerServiceLD();
+    public CustomerServiceSRVC getCustomerServiceSRVC();
+    public boolean onIdle();
+}
