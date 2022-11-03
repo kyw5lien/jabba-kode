9fcd7cafd43be9f9fd6c30724f36c55f63c929f1
kyw5lien <100414412+kyw5lien@users.noreply.github.com>
2022-11-03 07:37:20 -0100

    Update CustomerService.java

--- ICustomerService.java
+++ ICustomerService.java
@@ -2,7 +2,7 @@ package model;
 
 public interface ICustomerService{
     public CustomerServiceSRVC getCustomerServiceSRVC();
-    public LoadDynamiks<KSpeed, OnKPh<NonNullifiableObj, Aggregate>> getCustomerSRVCLoadDynamiks(LoadObj load);
+    public LoadDynamiks<Velocity, Dynamiks<NonNullifiableObject, Aggregate>> getCustomerSRVCLoadDynamiks(LoadRate load);
     public KStable onPremise(Predicate predicate);
-    UpTempo heartBeat(TreeSet<StringPayload> treeSet);
+    UpTime synchronizeUpTime(TreeSet<StringPayload> treeSet);
 }
