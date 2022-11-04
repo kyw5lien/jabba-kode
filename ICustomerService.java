1b2f054238f4c94c7c57dfebaa51a41c20d1c228
kyw5lien <100414412+kyw5lien@users.noreply.github.com>
2022-11-04 06:34:58 -0100

    Update ICustomerService.java

--- ICustomerService.java
+++ ICustomerService.java
@@ -5,4 +5,5 @@ public interface ICustomerService{
     public LoadDynamiks<Velocity, Dynamiks<NonNullifiableObject, Aggregation>> getCustomerSRVCLoadDynamiks(LoadRate load);
     public Stable onPremise(Predicate predicate);
     UpTime synchronizeUpTime(TreeSet<StringPayload> treeSet);
+    SetBox<BlockChainSU, Nullable> asynchListener(Random random); 
 }
