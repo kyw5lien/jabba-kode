9de9e2a690f8ce91da1cf123d03ef293ed4c05e2
kyw5lien <100414412+kyw5lien@users.noreply.github.com>
2022-11-28 19:17:57 -0100

    Update MatchingScenesViewer.java
    Update MatchingScenesViewer.java: updating the class to fit general usage purposes. Note: javadoc verbosity remains unsolved.

--- MatchingScenesViewer.java
+++ MatchingScenesViewer.java
@@ -1,16 +1,10 @@
-package cpp.mndl;
-
-import cpp.mndl.PerformativeScenes;
-import cpp.mndl.Theatre.TheatreScenes;
-
 public abstract class MatchingScenesViewer{
     /**
      * Returns the number of performative scenes that outstandingly match Theatre's Elaborated Scenes.
      *
      * @param scenes the performative scenes that should be outstandingly matched.
      * @param elaboratedScenes the theatre's elaborated scenes that scenes should be outstandingly matched against.
-     * @return a long, representing the number of scenes in the performative scenes that outstandingly match the elaboratedScenes.
-     * @implNote to request JAR files, contact Kap Loon E. Jr. at ccp-mindelo@camoes.mne.pt, and Follow The White John at centroculturaldomindelo@gmail.com .
+     * @return a long, representing the number of scenes in the performative scenes that outstandingly match the elaboratedScenes. .
      * @implSpec the implementation regards the theatre's actors and actresses performative sessions offered to the local public.
      */
     public static long countViewedScenes(PerformativeScenes<String> scenes, TheatreScenes<String> elaboratedScenes) {
