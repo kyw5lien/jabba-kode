75c2fb0c4745d292a8a48c65b7fbebf1e39b04fc
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2022-12-16 17:16:45 -0100

    Update AdaptiveScaler.java: reviewing methods and variable names.
    Update AdaptiveScaler.java: reviewing methods and variable names.

--- AdaptiveScaler.java
+++ AdaptiveScaler.java
@@ -1,9 +1,9 @@
-public interface AdaptativeScaler <T,E>{
-    public default <T> TimeLapseMomentum adaptativeScaling(T dataStructure, E adaptativeConstraintsSet) {
-        return AdaptativeScaler.constraintScheduling(DataStructureHandler.sortDataStructure(dataStructure), adaptativeConstraintsSet);
+public interface AdaptiveScaler<T,E>{
+    public default <T> TimeLapseMomentum adaptiveScaling(T dataStructure, E adaptiveConstraintSet) {
+        return AdaptiveScaler.constraintScheduling(DataStructureHandler.sortDataStructure(dataStructure), adaptiveConstraintSet);
     }
-    static <E> TimeLapseMomentum constraintScheduling(Object sortDataStructure, E adaptativeConstraintsSet) {
-        return ConstraintScheduler.schedule(sortDataStructure, adaptativeConstraintsSet);
+    static <E> TimeLapseMomentum constraintScheduling(Object sortedDataStructure, E adaptiveConstraintSet) {
+        return ConstraintScheduler.schedule(sortedDataStructure, adaptiveConstraintSet);
     }
     DataStructure sortDataStructure(T dataStructure);
 }
