feaa440a337a62238f8abd4be82d1aaf6249e293
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2022-12-16 18:38:07 -0100

    Update AdaptiveScaler.java: upgrading with ScalableDataStructure
    Update AdaptiveScaler.java: upgrading with a ScalableDataStructure to reflect the general need of DS' in solving real-world problems. Note: temp solution -> graph sets implementation needed.

--- AdaptiveScaler.java
+++ AdaptiveScaler.java
@@ -2,7 +2,7 @@ public interface AdaptiveScaler<T,E>{
     public default <T> TimeLapseMomentum adaptiveScaling(T dataStructure, E adaptiveConstraintSet) {
         return AdaptiveScaler.constraintScheduling(DataStructureHandler.sortDataStructure(dataStructure), adaptiveConstraintSet);
     }
-    static <E> TimeLapseMomentum constraintScheduling(Object sortedDataStructure, E adaptiveConstraintSet) {
+    static <E> TimeLapseMomentum constraintScheduling(ScalableDataStructure sortedDataStructure, E adaptiveConstraintSet) {
         return ConstraintScheduler.schedule(sortedDataStructure, adaptiveConstraintSet);
     }
     DataStructure sortDataStructure(T dataStructure);
