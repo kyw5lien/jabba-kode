7fd9b08b68265911253ca1578e7e7a877a02971b
Dario Neves <100414412+kyw5lien@users.noreply.github.com>
2022-12-16 12:09:07 -0100

    Update AdaptiveScaler.java: with a ConstraintScheduler class
    Update AdaptiveScaler.java: with a ConstraintScheduler class

--- AdaptiveScaler.java
+++ AdaptiveScaler.java
@@ -1,9 +1,9 @@
-public interface AdaptiveScaler <T,E>{
-    public default <T> TimeLapseMomentum adaptiveScaling(T dataStructure, E adaptiveConstraintsSet) {
-        return AdaptiveScaler.constraintScheduler(DataStructureHandler.sortDataStructure(dataStructure), adaptiveConstraintsSet);
+public interface AdaptativeScaler <T,E>{
+    public default <T> TimeLapseMomentum adaptativeScaling(T dataStructure, E adaptativeConstraintsSet) {
+        return AdaptativeScaler.constraintScheduling(DataStructureHandler.sortDataStructure(dataStructure), adaptativeConstraintsSet);
     }
-    static <E> TimeLapseMomentum constraintScheduler(Object sortDataStructure, E adaptiveConstraintsSet) {
-        return null;
+    static <E> TimeLapseMomentum constraintScheduling(Object sortDataStructure, E adaptativeConstraintsSet) {
+        return ConstraintScheduler.schedule(sortDataStructure, adaptativeConstraintsSet);
     }
     DataStructure sortDataStructure(T dataStructure);
 }
